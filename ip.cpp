a1cb790c7f4ac05332765c59ddf4c0d527f45b06
begin2ee <108245208+begin2ee@users.noreply.github.com>
2023-07-30 14:11:27 +0900

    Update ip.cpp

--- ip.cpp
+++ ip.cpp
@@ -1,15 +1,15 @@
 #include "ip.h"
 #include <cstdio>
 
-//Ip::Ip(const std::string r) {
-	/*unsigned int a, b, c, d;
+Ip::Ip(const std::string r) {
+	unsigned int a, b, c, d;
 	int res = sscanf(r.c_str(), "%u.%u.%u.%u", &a, &b, &c, &d);
 	if (res != SIZE) {
 		fprintf(stderr, "Ip::Ip sscanf return %d r=%s\n", res, r.c_str());
 		return;
 	}
-	ip_ = (a << 24) | (b << 16) | (c << 8) | d;*/
-//}
+	ip_ = (a << 24) | (b << 16) | (c << 8) | d;
+}
 
 Ip::operator std::string() const {
 	char buf[32]; // enough size
